2a9cc7faadbb86c07d279e3165ff4b7ad7e0a067
Anna <anna.mikhailovna305@gmail.com>
2020-12-10 01:13:15 +0300

    javafx || added

--- javafx/src/main/java/ru/itis/aleynik/example/Launcher.java
+++ javafx/src/main/java/ru/itis/aleynik/example/Launcher.java
@@ -1,4 +1,24 @@
 package ru.itis.aleynik.example;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        String fxml = "/fxml/Main.fxml";
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Example");
+
+        primaryStage.show();
+    }
 }
